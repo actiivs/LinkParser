b4423c16608a9b0a6b0fdc932d54f7b3d1e9fbec
actiivs <actiivs@gmail.com>
2014-12-13 17:49:43 +0800

    Creation

---
+++ LinkParser/Extension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinkParser
+{
+    public static class StringExtension
+    {
+        public static IEnumerable<int> AllIndexesOf(this string str, string value)
+        {
+            if (string.IsNullOrEmpty(str))
+                yield break;
+
+            if (String.IsNullOrEmpty(value))
+                throw new ArgumentException("the string to find may not be empty", "value");
+
+            for (int index = 0; ; index += value.Length)
+            {
+                index = str.IndexOf(value, index, StringComparison.Ordinal);
+                if (index == -1)
+                    break;
+                yield return index;
+            }
+        }
+    }
+
+    public static class EnumerableExtensions
+    {
+        private static readonly Random Rnd = new Random();
+
+        public static T Random<T>(this IEnumerable<T> input)
+        {
+            var enumerable = input as IList<T> ?? input.ToList();
+            return enumerable.ElementAt(Rnd.Next(enumerable.Count()));
+        }
+    }
+}
---
+++ LinkParser/MainWindow.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LinkParser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var aioLinks = new List<string>();
+
+            for (int i = int.Parse(StartIndex.Text); i <= int.Parse(EndIndex.Text); i++)
+            {
+                try
+                {
+                    var titles = await GetTitleUrl(i);
+                    foreach (var title in titles)
+                    {
+                        aioLinks.AddRange(await GetAiolinkUrl(title));
+                    }
+                }
+                catch(Exception){}
+            }
+
+            var ret = new List<string>();
+            foreach (var aioLink in aioLinks)
+            {
+                ret.AddRange(await GetFileHostLinks(aioLink));
+            }
+
+            File.WriteAllLines(string.Format("{0}-{1}.txt", StartIndex.Text, EndIndex.Text), ret);
+        }
+
+        private async Task<IEnumerable<string>> GetTitleUrl(int index)
+        {
+            var ret = new List<string>();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Referrer = new Uri("http://jav365.com");
+
+                using (var response = await client.GetAsync(string.Format("http://jav365.com/category/jav-censored/page/{0}/", index)))
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    const string startPattern = "<h3 class=\"entry-title\"><a href=\"";
+                    const string endPattern = "\" rel=\"bookmark\"";
+                    foreach (var searchStart in result.AllIndexesOf(startPattern))
+                    {
+                        var start = searchStart + startPattern.Length;
+                        var end = result.IndexOf(endPattern, start, StringComparison.Ordinal);
+                        if (end > 0)
+                        {
+                            ret.Add(result.Substring(start, end - start));
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+        
+        private async Task<IEnumerable<string>> GetLinkUrl(string titleUrl)
+        {
+            var ret = new List<string>();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Referrer = new Uri("http://jav365.com");
+
+                using (var response = await client.GetAsync(titleUrl))
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    const string startPattern = "http://jav365.com/dl";
+                    const string endPattern = "\"";
+                    foreach (var searchStart in result.AllIndexesOf(startPattern))
+                    {
+                        var start = searchStart;
+                        var end = result.IndexOf(endPattern, start, StringComparison.Ordinal);
+                        if (end > 0)
+                        {
+                            ret.Add(result.Substring(start, end - start));
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+
+        private async Task<IEnumerable<string>> GetAiolinkUrl(string titleUrl)
+        {
+            var ret = new List<string>();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Referrer = new Uri("http://jav365.com");
+
+                using (var response = await client.GetAsync(titleUrl))
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    const string startPattern = "http://aiolinks.com";
+                    const string endPattern = "\"";
+                    foreach (var searchStart in result.AllIndexesOf(startPattern))
+                    {
+                        var start = searchStart;
+                        var end = result.IndexOf(endPattern, start, StringComparison.Ordinal);
+                        if (end > 0)
+                        {
+                            ret.Add(result.Substring(start, end - start));
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+
+        private async Task<IEnumerable<string>> GetFileHostLinks(string aioLink)
+        {
+            var ret = new List<string>();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Referrer = new Uri("http://jav365.com");
+
+                using (var response = await client.GetAsync(aioLink))
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    var startPattern = "http://rapidgator.net";
+                    const string endPattern = "\"";
+                    foreach (var searchStart in result.AllIndexesOf(startPattern))
+                    {
+                        var start = searchStart;
+                        var end = result.IndexOf(endPattern, start, StringComparison.Ordinal);
+                        if (end > 0)
+                        {
+                            ret.Add(result.Substring(start, end - start));
+                        }
+                    }
+
+                    startPattern = "http://www.uploadable.ch";
+                    foreach (var searchStart in result.AllIndexesOf(startPattern))
+                    {
+                        var start = searchStart;
+                        var end = result.IndexOf(endPattern, start, StringComparison.Ordinal);
+                        if (end > 0)
+                        {
+                            ret.Add(result.Substring(start, end - start));
+                        }
+                    }
+
+                    startPattern = "http://uploadable.ch";
+                    foreach (var searchStart in result.AllIndexesOf(startPattern))
+                    {
+                        var start = searchStart;
+                        var end = result.IndexOf(endPattern, start, StringComparison.Ordinal);
+                        if (end > 0)
+                        {
+                            ret.Add(result.Substring(start, end - start));
+                        }
+                    }
+
+                    startPattern = "http://bitshare.com";
+                    foreach (var searchStart in result.AllIndexesOf(startPattern))
+                    {
+                        var start = searchStart;
+                        var end = result.IndexOf(endPattern, start, StringComparison.Ordinal);
+                        if (end > 0)
+                        {
+                            ret.Add(result.Substring(start, end - start));
+                        }
+                    }
+
+                    startPattern = "http://freakshare.com";
+                    foreach (var searchStart in result.AllIndexesOf(startPattern))
+                    {
+                        var start = searchStart;
+                        var end = result.IndexOf(endPattern, start, StringComparison.Ordinal);
+                        if (end > 0)
+                        {
+                            ret.Add(result.Substring(start, end - start));
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
